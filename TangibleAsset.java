4afcc362e8d6f3a03696d16673eee7947c681361
Nakahara76 <nakahara6835@gmail.com>
2022-05-09 18:52:40 +0900

    Create Computer.java naoshikatawakaranai

--- TangibleAsset.java
+++ TangibleAsset.java
@@ -9,8 +9,8 @@ public abstract class TangibleAsset {
     this.color = color;
   }
   
-  public String getName() { return this.name }
-  public int getPrice() { return this.price }
-  public String getColor() { return this.color }
+  public String getName() { return this.name; }
+  public int getPrice() { return this.price; }
+  public String getColor() { return this.color; }
   
 }
